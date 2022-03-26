681a4c4a954bfba561390465a8b4fce8f6b0397b
Jase <the.root.user@gmail.com>
2022-03-26 14:50:11 -0400

    Basic Random Winner Selector version alpha

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import {useEffect, useState } from 'react';
+import FormGroup from '@mui/material/FormGroup'
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+
 import './App.css';
 
+
 function App() {
+  const [names, setNames] = useState([])
+  const [fieldEntry, setField] = useState('')
+  useEffect(() => {
+
+  }, [fieldEntry])
+  
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (fieldEntry.length < 1) {
+      return null
+    }
+    setNames([{ name: fieldEntry, winner: false }, ...names])
+    return setField('')
+  }
+
+  const pickHandler = () => {
+    console.log('fire')
+    const random = Math.floor(Math.random() * names.length)
+
+    setNames(names.map((obj, idx) => {
+      obj.winner = false
+      return obj
+    }))
+
+    setNames(names.map((obj, idx) => {
+      if (idx === random) {
+        obj.winner = !obj.winner
+      }
+      return obj
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => submitHandler(e)}>
+      <FormGroup>
+        <List>
+          {names.length > 0 ? names.map((obj, idx) => <ListItem key={`${idx}-${obj.name}`} className={obj.winner ? 'winner' : 'loser'}>{obj.name}</ListItem>) : <ListItem>No Names</ListItem>}
+        </List>
+        <FormControl>
+          <InputLabel htmlFor='user' />
+          <Input id='user' onChange={(e) => setField(e.currentTarget.value)} value={fieldEntry} placeholder="Enter A Name" />
+          <ButtonGroup variant="outline" aria-label="outlined primary button group">
+            <Button type='submit'>Submit</Button>
+            <Button onClick={pickHandler}>Pick Winner</Button>
+          </ButtonGroup>
+        </FormControl>
+      </FormGroup>
+      </form>
     </div>
   );
 }
